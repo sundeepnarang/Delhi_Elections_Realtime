b28ba60eaefe3bec006b4867358f5cd23535fdb0
Sundeep Narang <sundeepnrng@gmail.com>
2015-02-10 00:01:57 -0500

    step

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,16 @@ var express = require('express');
 var cheerio = require('cheerio'),
     request = require('request');
 var router = express.Router();
+var Step = require('step');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-    request({uri: 'http://eciresults.nic.in/StatewiseU05.htm'}, function(err, response, body){
+function getData(url,done){
+    request({uri: url}, function(err, response, body){
 
         $ = cheerio.load(body);
         var data = [];
-        $('#divACList tr').each(function(i, tr){
-            if((i>=17)&&(i<=26)){
+        var trs =$('#divACList tr');
+        trs.each(function(i, tr){
+            if($(this).children().length>6){
 
                 var children = $(this).children();
                 var ConstituencyItem = children.eq(0);
@@ -26,12 +27,36 @@ router.get('/', function(req, res) {
                     "Margin": MarginItem.text().trim(),
                     "Status": StatusItem.text().trim(),
                 };
-                data.push(row);
+                if(row.Constituency!="Constituency"){
+                    data.push(row);
+                }
+
                 console.log(row);
             }
         });
-        res.send(data);
+        done(data);
     });
+}
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    Step(
+        // Loads two files in parallel
+        function loadStuff() {
+            getData('http://eciresults.nic.in/StatewiseU05.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU051.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU052.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU053.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU054.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU055.htm', this.parallel());
+            getData('http://eciresults.nic.in/StatewiseU056.htm', this.parallel());
+        },
+        // Show the result when done
+        function showStuff(err, one, two, three, four,five,six,seven) {
+            if (err) console.log(err);
+
+        }
+    )
 });
 
 module.exports = router;
